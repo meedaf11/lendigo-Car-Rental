342c319a84c6bb57ed867365f0e80ccc91caba39
meedaf11 <152024289+meedaf11@users.noreply.github.com>
2025-05-26 20:40:42 +0100

    database connection

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+$db_host = 'localhost';
+$db_name = 'lendigodb';
+$db_username = 'root';
+$db_password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_username, $db_password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+} catch (PDOException $e) {
+    die('Database connection failed');
+}
+?>
